e6285efc02063a3d4987e1a2100f8fb7aa9b8baa
Will413028 <will413028@gmail.com>
2023-02-02 15:08:08 +0800

    feat: hashing password with bcrypt

--- alembic/versions/d451043f6f04_create_user_table.py
+++ alembic/versions/d451043f6f04_create_user_table.py
@@ -7,6 +7,7 @@ Create Date: 2023-01-30 16:31:10.789768
 """
 from alembic import op
 import sqlalchemy as sa
+from utils.hash import Hash
 
 
 # revision identifiers, used by Alembic.
@@ -26,11 +27,11 @@ def upgrade() -> None:
 
     op.bulk_insert(user,
     [
-        {'id':1, 'username':'user1', 'email':'user1@test.com', 'password':'12345678' },
-        {'id':2, 'username':'user2', 'email':'user2@test.com', 'password':'12345678' },
-        {'id':3, 'username':'user3', 'email':'user3@test.com', 'password':'12345678' },
-        {'id':4, 'username':'user4', 'email':'user4@test.com', 'password':'12345678' },
-        {'id':5, 'username':'user5', 'email':'user5@test.com', 'password':'12345678' }
+        {'id':1, 'username':'user1', 'email':'user1@test.com', 'password':Hash.bcrypt('12345678')},
+        {'id':2, 'username':'user2', 'email':'user2@test.com', 'password':Hash.bcrypt('12345678')},
+        {'id':3, 'username':'user3', 'email':'user3@test.com', 'password':Hash.bcrypt('12345678')},
+        {'id':4, 'username':'user4', 'email':'user4@test.com', 'password':Hash.bcrypt('12345678')},
+        {'id':5, 'username':'user5', 'email':'user5@test.com', 'password':Hash.bcrypt('12345678')}
     ]
 )
 
--- core/user.py
+++ core/user.py
@@ -1,20 +1,20 @@
 from schemas.user import UserBase
 from database.models import User
 from sqlalchemy.orm.session import Session
+from utils.hash import Hash
 
 
 def create_user(db: Session, request: UserBase):
     new_user = User(
         username = request.username,
         email = request.email,
-        password = request.password
+        password = Hash.bcrypt(request.password)
     )
     try:
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
+        return new_user
     except Exception as e:
         db.rollback()
         raise Exception(str(e))
-    return new_user
-
