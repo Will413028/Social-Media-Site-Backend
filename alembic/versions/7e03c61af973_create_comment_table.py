5e1e575602a6946de9f0d63dc51bfe1bfc2ecbfc
Will413028 <will413028@gmail.com>
2023-02-06 12:49:57 +0800

    feat: add comment table

--- alembic/versions/7e03c61af973_create_comment_table.py
+++ alembic/versions/7e03c61af973_create_comment_table.py
@@ -1,8 +1,8 @@
-"""create post table
+"""create comment table
 
-Revision ID: e702a3ecf8c3
-Revises: d451043f6f04
-Create Date: 2023-02-03 11:18:32.875482
+Revision ID: 7e03c61af973
+Revises: e702a3ecf8c3
+Create Date: 2023-02-06 11:41:15.335538
 
 """
 from alembic import op
@@ -10,23 +10,22 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e702a3ecf8c3'
-down_revision = 'd451043f6f04'
+revision = '7e03c61af973'
+down_revision = 'e702a3ecf8c3'
 branch_labels = None
 depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('post',
+    op.create_table('comment',
     sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('image_url', sa.String(255), nullable=True),
-    sa.Column('image_url_type', sa.String(36), nullable=True),
-    sa.Column('caption', sa.String(36), nullable=True),
+    sa.Column('text', sa.String(255), nullable=True),
     sa.Column('timestamp', sa.DateTime, nullable=True),
+    sa.Column('post_id', sa.Integer, nullable=True),
+    sa.ForeignKeyConstraint(['post_id'], ['post.id'], ),
     sa.Column('user_id', sa.Integer, nullable=True),
     sa.ForeignKeyConstraint(['user_id'], ['user.id'], )
     )
 
-
 def downgrade() -> None:
-    op.drop_table('post')
+    op.drop_table('comment')
--- database/models.py
+++ database/models.py
@@ -10,7 +10,7 @@ class User(Base):
     email = Column(String(36))
     password = Column(String(128))
     posts = relationship('Post', back_populates='user')
-
+    comments = relationship('Comment', back_populates='user')
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True, index=True)
@@ -20,3 +20,14 @@ class Post(Base):
     timestamp = Column(DateTime)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship('User', back_populates="posts")
+    comments = relationship('Comment', back_populates="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String(255))
+    timestamp = Column(DateTime)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post', back_populates="comments")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates="comments")
