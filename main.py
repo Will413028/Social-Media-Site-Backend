79c47434493e529597e7e4e7cf2832154fc6aa5e
Will413028 <will413028@gmail.com>
2023-02-06 13:16:17 +0800

    feat: add CORS middle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routers import user, post, comment, authentication
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -9,5 +9,15 @@ app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(comment.router)
 app.include_router(authentication.router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
